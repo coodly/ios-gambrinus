f6c5b8e1fbe3809a216ec4aad3ac325534756105
Jaanus Siim <jaanus@jaanussiim.com>
2015-05-29 20:14:59 +0300

    Use autolayout in post cell

--- Gambrinus/Cells/PostCell.m
+++ Gambrinus/Cells/PostCell.m
@@ -24,7 +24,7 @@
 @property (nonatomic, strong) IBOutlet UIView *titleBackground;
 @property (nonatomic, strong) IBOutlet UIButton *addRemoveButton;
 @property (nonatomic, strong) IBOutlet UILabel *dateLabel;
-@property (nonatomic, strong) UIView *dateBackground;
+@property (nonatomic, strong) IBOutlet UIView *dateBackground;
 
 @end
 
@@ -49,16 +49,11 @@
     [self.titleLabel setTextColor:[UIColor myOrange]];
     [self.dateLabel setTextColor:[UIColor myOrange]];
     [self.layer setCornerRadius:5];
-    UIView *titleBackground = [[UIView alloc] initWithFrame:self.bounds];
-    [self setTitleBackground:titleBackground];
-    [titleBackground setBackgroundColor:[UIColor colorWithWhite:1 alpha:0.8]];
-    [self.contentView insertSubview:titleBackground belowSubview:self.titleLabel];
+
+    [self.titleBackground setBackgroundColor:[UIColor colorWithWhite:1 alpha:0.8]];
     [self.titleLabel setBackgroundColor:[UIColor clearColor]];
 
-    UIView *dateBackground = [[UIView alloc] initWithFrame:self.bounds];
-    [self setDateBackground:dateBackground];
-    [dateBackground setBackgroundColor:[UIColor colorWithWhite:1 alpha:0.8]];
-    [self.contentView insertSubview:dateBackground belowSubview:self.dateLabel];
+    [self.dateBackground setBackgroundColor:[UIColor colorWithWhite:1 alpha:0.8]];
     [self.dateLabel setBackgroundColor:[UIColor clearColor]];
 
     [self.addRemoveButton.layer setCornerRadius:5];
@@ -70,22 +65,13 @@
 - (void)adjustContentFont:(NSNotification *)notification {
     [self.titleLabel setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleHeadline]];
     [self.dateLabel setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleCaption1]];
+    [self layoutIfNeeded];
 }
 
 - (void)setTitle:(NSString *)title {
     [self.titleLabel setText:title];
     [self.titleLabel setNumberOfLines:2];
     [self.titleLabel setMinimumScaleFactor:0.5];
-    CGFloat fitHeight = [self.titleLabel sizeThatFits:CGSizeMake(CGRectGetWidth(self.titleLabel.frame), CGFLOAT_MAX)].height;
-
-    CGRect labelFrame = self.titleLabel.bounds;
-    labelFrame.size.height = fitHeight;
-    [self.titleLabel setFrame:CGRectOffset(labelFrame, 5, CGRectGetHeight(self.bounds) - CGRectGetHeight(labelFrame))];
-
-    CGRect backgroundFrame = self.titleBackground.frame;
-    backgroundFrame.origin.y = self.titleLabel.frame.origin.y;
-    backgroundFrame.size.height = labelFrame.size.height;
-    [self.titleBackground setFrame:backgroundFrame];
 }
 
 - (void)setDateString:(NSString *)dateString {
@@ -96,14 +82,6 @@
     }
 
     [self.dateLabel setText:dateString];
-    [self.dateLabel sizeToFit];
-
-    CGRect backgroundFrame = self.dateBackground.frame;
-    backgroundFrame.origin.x = 0;
-    backgroundFrame.origin.y = 0;
-    backgroundFrame.size.width = CGRectGetWidth(self.contentView.frame);
-    backgroundFrame.size.height = CGRectGetHeight(self.dateLabel.frame);
-    [self.dateBackground setFrame:backgroundFrame];
 }
 
 - (IBAction)addRemoveTapped {
