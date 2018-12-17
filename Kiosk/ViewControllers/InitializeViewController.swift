12ba51fb08ee26dc5c99a995255fc811a59dd9f1
Jaanus Siim <jaanus@jaanussiim.com>
2018-12-17 18:24:37 +0200

    Add Kiosk target

---
+++ Kiosk/ViewControllers/InitializeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  InitializeViewController.swift
+//  Kiosk
+//
+//  Created by Jaanus Siim on 17/12/2018.
+//  Copyright © 2018 Coodly LLC. All rights reserved.
+//
+
+import UIKit
+
+internal class InitializeViewController: UIViewController {
+
+}
