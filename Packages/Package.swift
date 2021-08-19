1299159fcc284760a93e22cec103b9343d418343
Jaanus Siim <jaanus@jaanussiim.com>
2021-08-19 08:07:30 +0300

    Define dependencies in package

--- Packages/Package.swift
+++ Packages/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Packages",
+    platforms: [.iOS(.v9)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -12,8 +13,11 @@ let package = Package(
             targets: ["Packages"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/coodly/BloggerAPI.git", .exact("0.1.1")),
+        .package(url: "https://github.com/coodly/CoreDataPersistence.git", .exact("0.2.4")),
+        .package(url: "https://github.com/coodly/ImageProvide.git", .exact("0.5.0")),
+        .package(url: "https://github.com/coodly/Puff.git", .exact("0.6.3")),
+        .package(url: "https://github.com/coodly/swlogger.git", .exact("0.6.2")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
